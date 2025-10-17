335aa691d4d62628660649701207c48c6825321b
Shashank Jadhav <132274771+Shashank1394@users.noreply.github.com>
2025-10-17 15:43:50 +0530

    Add MongoDB connection to server initialization

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.send("TaskFlow API running!");
 });
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.log("DB Error", err));
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
